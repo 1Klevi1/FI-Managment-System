9c1204a7257213f41239afa5a35112e6b164fe02
Klevi Alliu <klevialliu4@gmail.com>
2025-04-22 16:25:23 +0100

    adding the database file (sqlite)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from dashboard import MainDashboard  # Import the MainDashboard class from dashboard.py
+from fleet_database import init_db, read_fleet_data, add_vehicle, update_vehicle, delete_vehicle
 
 
 def main():
@@ -8,4 +9,6 @@ def main():
     root.mainloop()
 
 if __name__ == "__main__":
+    init_db()  # Initialize DB and table if not already existing
     main()
+
